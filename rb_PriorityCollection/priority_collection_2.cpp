480c349ff44350eda70e1a62fc7d7956b9f8619b
HoneyBad3er <53121036+HoneyBad3er@users.noreply.github.com>
2019-07-23 17:25:41 +0300

    Add files via upload

---
+++ rb_PriorityCollection/priority_collection_2.cpp
@@ -0,0 +1,123 @@
+#include "test_runner.h"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <set>
+#include <utility>
+#include <list>
+
+using namespace std;
+
+template <typename T>
+class PriorityCollection {
+public:
+  using Id = size_t;
+
+  // Добавить объект с нулевым приоритетом
+  // с помощью перемещения и вернуть его идентификатор
+    Id Add(T object){
+        data.push_back(make_pair(move(object), 0));
+        Id index = data.size() - 1;
+        rate.insert(make_pair(0, index));
+        ids.insert(index);
+        return index;
+    }
+
+  // Добавить все элементы диапазона [range_begin, range_end)
+  // с помощью перемещения, записав выданные им идентификаторы
+  // в диапазон [ids_begin, ...)
+  template <typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end,
+           IdOutputIt ids_begin){
+      vector<Id> ids;
+      for (auto it = make_move_iterator(range_begin); it != make_move_iterator(range_end); ++it ){
+          ids.push_back(Add(*it));
+      }
+        copy(ids.begin(), ids.end(), ids_begin);
+      }
+
+  // Определить, принадлежит ли идентификатор какому-либо
+  // хранящемуся в контейнере объекту
+  bool IsValid(Id id) const{
+      return id > data.size() || ids.find(id) == ids.end()? false : true;
+  }
+
+  // Получить объект по идентификатору
+  const T& Get(Id id) const{
+      return (data[id]).first;
+  }
+
+  // Увеличить приоритет объекта на 1
+  void Promote(Id id){
+        rate.erase(make_pair(data[id].second,id));
+        ++data[id].second;
+        rate.insert(make_pair(data[id].second,id));
+  }
+
+  // Получить объект с максимальным приоритетом и его приоритет
+  pair<const T&, int> GetMax() const{
+      auto last = rate.end();
+      --last;
+      Id max_id = (*last).second;
+      return data[max_id];
+  }
+
+  // Аналогично GetMax, но удаляет элемент из контейнера
+  pair<T, int> PopMax(){
+      auto last = rate.end();
+      --last;
+      Id max_id = (*last).second;
+      ids.erase(max_id);
+      rate.erase(last);
+      return move(data[max_id]);
+  }
+private:
+set<pair<int, Id>> rate;
+vector<pair<T, int>> data;
+set<Id> ids;
+};
+
+
+class StringNonCopyable : public string {
+public:
+  using string::string;  // Позволяет использовать конструкторы строки
+  StringNonCopyable(const StringNonCopyable&) = delete;
+  StringNonCopyable(StringNonCopyable&&) = default;
+  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy() {
+  PriorityCollection<StringNonCopyable> strings;
+  const auto white_id = strings.Add("white");
+  const auto yellow_id = strings.Add("yellow");
+  const auto red_id = strings.Add("red");
+
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
+  }
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+  }
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestNoCopy);
+  return 0;
+}
